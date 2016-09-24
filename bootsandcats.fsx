5d573c719e1373f75aaf5fbe8e4e3683e3941575
MiyamotoAkira <yefo.akira@gmail.com>
2016-09-24 13:09:56 +0100

    First stab at logic
    A first try with the logic of the game has been added

--- bootsandcats.fsx
+++ bootsandcats.fsx
@@ -32,3 +32,72 @@ module Keyboard =
     let init() =
         document.addEventListener_keydown(fun e -> update (e, true))
         document.addEventListener_keyup(fun e-> update(e, false))
+
+
+let calculatevx force angle  = force *  (cos angle)
+let calculatevy force angle  = force *  (sin angle)
+
+let g = 9.8
+
+type Wellie =
+    { x: float;
+      y: float;
+      vx: float;
+      vy: float;
+    }
+
+let calculateWelliePosition wellie (originalForce: float)  (originalAngle :float) (time:float) =
+    if wellie.y = 0. && wellie.vy <= 0.
+    then
+        { wellie with vy = 0.; vx = 0. }
+    else
+        {
+            x = originalForce * time * (cos originalAngle);
+            y = originalForce * time * (sin originalAngle) - (1./2.) * g * (time**2);
+            vx = originalForce * time * (cos originalAngle)
+            vy = originalForce  * (sin originalAngle) - g * time
+        }
+
+type Cat =
+    { x: float;
+      img: string}
+
+type Gamestate =
+    | Initial
+    | InProgress of (Cat * Wellie * int * float * float * float)
+    | Finished
+
+let moveCat cat wellie =
+    cat
+
+let calculateCatPosition cat wellie =
+    if wellie.y = 0. && wellie.vy = 0. then
+        if abs (cat.x - wellie.x) < 0. then
+          moveCat cat wellie
+        else
+          cat
+    else
+        cat
+
+let calculateNewGameState wellie numberOfWellies originalForce originalAngle time (cat:Cat) =
+    if numberOfWellies = 0 then Finished
+    else if cat.x > 100. then Finished
+    else InProgress (cat, wellie, numberOfWellies, originalForce, originalAngle, time)
+    
+
+let rec gameloop gamestate =
+    match gamestate with
+        | Finished -> gamestate
+        | Initial ->
+            let cat = { x= 50.; img = ""}
+            let angle = 45.
+            let force = 50.
+            let vx' = 0.;
+            let vy' = 0.;
+            gameloop (InProgress (cat, {x = 0.; y = 0.; vx = vx'; vy = vy'}, 5, force, angle, 0.))
+        | InProgress (cat, wellie, numberOfWellies, originalForce, originalAngle, time) ->
+            let wellie' =calculateWelliePosition wellie  originalForce  originalAngle  time
+            let gamestate' =
+                calculateCatPosition cat wellie'
+                |> calculateNewGameState wellie' numberOfWellies originalForce originalAngle time
+            gameloop gamestate'
